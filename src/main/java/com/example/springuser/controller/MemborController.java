20ed30aa1dc72630f9cc51537ccdaa94c5bf38c0
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 21:12:58 +0900

    feat : login 페이지, 컨트롤러 추가(강의#06)

--- src/main/java/com/example/springuser/controller/MemborController.java
+++ src/main/java/com/example/springuser/controller/MemborController.java
@@ -2,14 +2,13 @@ package com.example.springuser.controller;
 
 import com.example.springuser.dto.MemberDTO;
 import com.example.springuser.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.lang.reflect.Member;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,15 +17,31 @@ public class MemborController {
     private final MemberService memberService; //@RequiredArgsConstructor 에서 생성해줌.
 
     @GetMapping("/member/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
 
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
         return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm(){
+        return "login";
+    }
+
+    @PostMapping("member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            return "login";
+        }
     }
 }
