5b2de2e93eae93f99287a4aa79ed9036e7047142
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 21:12:34 +0900

    feat : toMemberDto 함수 추가(강의#06)

--- src/main/java/com/example/springuser/dto/MemberDTO.java
+++ src/main/java/com/example/springuser/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.example.springuser.dto;
 
+import com.example.springuser.entity.MemberEntity;
 import lombok.*;
 
 @Getter
@@ -12,4 +13,13 @@ public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
     private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        return memberDTO;
+    }
 }
