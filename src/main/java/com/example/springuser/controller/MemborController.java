eebf6a0b84fe32674715e62201f94f7128054375
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 17:49:45 +0900

    feat : /member/save post 컨트롤러 추가(강의#05)

--- src/main/java/com/example/springuser/controller/MemborController.java
+++ src/main/java/com/example/springuser/controller/MemborController.java
@@ -2,11 +2,25 @@ package com.example.springuser.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemborController {
     @GetMapping("/member/save")
     public String saveForm(){
         return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName){
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail);
+        System.out.println("memberPassword = " + memberPassword);
+        System.out.println("memberName = " + memberName);
+
+        return "index";
     }
 }
