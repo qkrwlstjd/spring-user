c4603fdde615d205ee617ba51a13becbb12d02a0
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 21:27:16 +0900

    feat : member list 조회 컨트롤러 추가 (강의#07)

--- src/main/java/com/example/springuser/controller/MemborController.java
+++ src/main/java/com/example/springuser/controller/MemborController.java
@@ -5,9 +5,12 @@ import com.example.springuser.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -30,7 +33,7 @@ public class MemborController {
     }
 
     @GetMapping("/member/login")
-    public String loginForm(){
+    public String loginForm() {
         return "login";
     }
 
@@ -43,5 +46,14 @@ public class MemborController {
         } else {
             return "login";
         }
+    }
+
+    @GetMapping("/member")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+
+        //html로 데이터 전송
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
     }
 }
