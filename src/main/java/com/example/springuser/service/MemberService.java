69574bb17375e201d9403fada25d2e0668e05154
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 21:13:27 +0900

    feat : login 서비스 추가(강의#06)

--- src/main/java/com/example/springuser/service/MemberService.java
+++ src/main/java/com/example/springuser/service/MemberService.java
@@ -6,6 +6,8 @@ import com.example.springuser.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -14,5 +16,23 @@ public class MemberService {
     public void save(MemberDTO memberDTO) {
         MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity);
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+        if (byMemberEmail.isPresent()) {
+            //isPresent=존재한다.
+            MemberEntity memberEntity = byMemberEmail.get();
+            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                //java 문자열 비교할때 equals 사용해야함. ==는 주소값 비교
+                MemberDTO loginUser = MemberDTO.toMemberDTO(memberEntity);
+                return loginUser;
+            } else {
+                return null;
+            }
+
+        } else {
+            return null;
+        }
     }
 }
