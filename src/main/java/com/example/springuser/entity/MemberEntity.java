21e39fffc72e08e36dbb71246130fadb9ead5ef0
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 20:16:31 +0900

    feat : toMemberEntity함수 추가 (강의#05_4)
    Repository.save 매개변수=Entity기 때문

--- src/main/java/com/example/springuser/entity/MemberEntity.java
+++ src/main/java/com/example/springuser/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.example.springuser.entity;
 
+import com.example.springuser.dto.MemberDTO;
 import jakarta.persistence.*;
 
 import lombok.Getter;
@@ -22,4 +23,12 @@ public class MemberEntity {
 
     @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO){
+        MemberEntity memberEntity=new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
 }
