83cab150391140b9d2f8cc6e0421a583a211d9e9
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 20:03:32 +0900

    feat : memberDto로 파라미터 받게 수정 (강의#05_3)

--- src/main/java/com/example/springuser/controller/MemborController.java
+++ src/main/java/com/example/springuser/controller/MemborController.java
@@ -1,26 +1,32 @@
 package com.example.springuser.controller;
 
+import com.example.springuser.dto.MemberDTO;
+import com.example.springuser.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Member;
+
 @Controller
+@RequiredArgsConstructor
 public class MemborController {
+    //생성자 주입
+    private final MemberService memberService; //@RequiredArgsConstructor 에서 생성해줌.
+
     @GetMapping("/member/save")
     public String saveForm(){
         return "save";
     }
 
     @PostMapping("/member/save")
-    public String save(@RequestParam("memberEmail") String memberEmail,
-                       @RequestParam("memberPassword") String memberPassword,
-                       @RequestParam("memberName") String memberName){
+    public String save(@ModelAttribute MemberDTO memberDTO){
         System.out.println("MemberController.save");
-        System.out.println("memberEmail = " + memberEmail);
-        System.out.println("memberPassword = " + memberPassword);
-        System.out.println("memberName = " + memberName);
-
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
         return "index";
     }
 }
