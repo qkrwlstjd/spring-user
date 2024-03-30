37a8a03bc6096d500503fe5d8d5deb47938f6c80
qkrwlstjd <qkrwls91877276@gmail.com>
2024-03-30 21:13:46 +0900

    feat : MemberRepository.findByMemberEmail 추가(강의#06)

--- src/main/java/com/example/springuser/repository/MemberRepository.java
+++ src/main/java/com/example/springuser/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.example.springuser.repository;
 import com.example.springuser.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
 }
